5081a7419ee8ef6ac5e19b40c003d134927eec23
felipemichelin <contato@felipemichelin.com>
2022-07-09 11:21:31 -0300

    a

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'drink-javy.myshopify.com',
+    storefrontToken: 'e7798126aeb22dcfbf534f0a8036163f',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
